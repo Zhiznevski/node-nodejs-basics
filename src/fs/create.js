a7d1d1a8111eb18e4fe09f02934fca9059bcd7cc
ZhiznevskiyArtem <zhiznevskiyartem@gmail.com>
2025-07-15 14:24:59 +0300

    feat: add create.js fs function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import fs from "node:fs/promises";
+
+const FILE_NAME = "fresh.txt";
+const FILE_CONTENT = "I am fresh and young";
+const FILE_PATH = `./src/fs/files/${FILE_NAME}`;
+const ERROR_MESSAGE = "FS operation failed";
+
 const create = async () => {
-  // Write your code here
+  try {
+    await fs.writeFile(FILE_PATH, FILE_CONTENT, { flag: "wx" });
+  } catch (err) {
+    if (err.code === "EEXIST") {
+      throw new Error(ERROR_MESSAGE);
+    }
+    console.error(err);
+  }
 };
 
 await create();
