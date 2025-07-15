78df18412bc3bb3856bbf6160d23e20605e33783
ZhiznevskiyArtem <zhiznevskiyartem@gmail.com>
2025-07-15 20:41:58 +0300

    feat: add env.js function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-  // Write your code here
+  let res = "";
+  for (const v in process.env) {
+    if (!v.startsWith("RSS_")) continue;
+    res += `${v}=${process.env[v]}; `;
+  }
+
+  console.log(res.trim().slice(0, -1));
 };
 
 parseEnv();
