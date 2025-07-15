7a9fff79f7da1d6593ffb87b6b6fac52ef19b898
ZhiznevskiyArtem <zhiznevskiyartem@gmail.com>
2025-07-15 16:06:17 +0300

    feat: add delete.js fs function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
+import { rm } from "node:fs/promises";
+import path from "node:path";
+
+const FILE_NAME = "fileToRemove.txt";
+const FILE_PATH = path.join("src", "fs", "files", FILE_NAME);
+const ERROR_MESSAGE = "FS operation failed";
+
 const remove = async () => {
-  // Write your code here
+  try {
+    await rm(FILE_PATH);
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      throw new Error(ERROR_MESSAGE);
+    }
+    throw err;
+  }
 };
 
 await remove();
