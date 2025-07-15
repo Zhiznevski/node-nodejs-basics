b859afeed7e9168408f9b9bc06d751ff8321931e
ZhiznevskiyArtem <zhiznevskiyartem@gmail.com>
2025-07-15 16:23:21 +0300

    feat: add list.js fs function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import { readdir } from "node:fs/promises";
+import path from "node:path";
+
+const FOLDER_PATH = path.join("src", "fs", "files");
+const ERROR_MESSAGE = "FS operation failed";
+
 const list = async () => {
-  // Write your code here
+  try {
+    const files = await readdir(FOLDER_PATH);
+    for (const file of files) console.log(file);
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      throw new Error(ERROR_MESSAGE);
+    }
+    throw err;
+  }
 };
 
 await list();
