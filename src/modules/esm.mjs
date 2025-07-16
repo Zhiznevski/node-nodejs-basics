27c270e2f566bb3edec8053d63e874b97194307b
ZhiznevskiyArtem <zhiznevskiyartem@gmail.com>
2025-07-16 13:26:22 +0300

    feat: add esm.mjs modules function

---
+++ src/modules/esm.mjs
@@ -0,0 +1,71 @@
+import path from "node:path";
+import { release, version } from "node:os";
+import { createServer as createServerHttp } from "node:http";
+import url from 'node:url';
+
+import "./files/c.cjs";
+
+import aJson from "./files/a.json" with { type: "json" };
+import bJson from "./files/b.json" with { type: "json" };
+
+const random = Math.random();
+
+const unknownObject = random > 0.5 ? aJson : bJson;
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${path.sep}"`);
+
+console.log(`Path to current file is ${url.fileURLToPath(import.meta.url)}`);
+console.log(`Path to current directory is ${path.dirname(url.fileURLToPath(import.meta.url))}`);
+
+const myServer = createServerHttp((_, res) => {
+  res.end("Request accepted");
+});
+
+const PORT = 3000;
+
+console.log(unknownObject);
+
+myServer.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+  console.log("To terminate it, use Ctrl+C combination");
+});
+
+export { unknownObject, myServer };
+
+// const path = require("node:path");
+// const { release, version } = require("node:os");
+// const { createServer: createServerHttp } = require("node:http");
+
+// require("./files/c.cjs");
+
+// const random = Math.random();
+
+// const unknownObject =
+//   random > 0.5 ? require("./files/a.json") : require("./files/b.json");
+
+// console.log(`Release ${release()}`);
+// console.log(`Version ${version()}`);
+// console.log(`Path segment separator is "${path.sep}"`);
+
+// console.log(`Path to current file is ${__filename}`);
+// console.log(`Path to current directory is ${__dirname}`);
+
+// const myServer = createServerHttp((_, res) => {
+//   res.end("Request accepted");
+// });
+
+// const PORT = 3000;
+
+// console.log(unknownObject);
+
+// myServer.listen(PORT, () => {
+//   console.log(`Server is listening on port ${PORT}`);
+//   console.log("To terminate it, use Ctrl+C combination");
+// });
+
+// module.exports = {
+//   unknownObject,
+//   myServer,
+// };
