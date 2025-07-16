e674e706455417556e3afbbc7d5db0eff84299ad
ZhiznevskiyArtem <zhiznevskiyartem@gmail.com>
2025-07-16 15:41:35 +0300

    feat: add calcHash.js hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+const { createHash } = await import("node:crypto");
+import { createReadStream } from "node:fs";
+import { join } from "node:path";
+
+const FILE_NAME = "fileToCalculateHashFor.txt";
+const FILE_PATH = join("src", "hash", "files", FILE_NAME);
+const hash = createHash("sha256");
+
+const input = createReadStream(FILE_PATH);
+
 const calculateHash = async () => {
-  // Write your code here
+  input.on("readable", () => {
+    const data = input.read();
+    if (data) hash.update(data);
+    else {
+      console.log(`${hash.digest("hex")}`);
+    }
+  });
 };
 
 await calculateHash();
