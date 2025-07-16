6d4faaa8692b8fbc9fc4dbf59d2da6d4f3bfbb49
ZhiznevskiyArtem <zhiznevskiyartem@gmail.com>
2025-07-16 16:49:17 +0300

    feat: add write.js stream function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import { createWriteStream } from "node:fs";
+import { stdin } from "node:process";
+import { join } from "node:path";
+import { pipeline } from "node:stream/promises";
+
+const FILE_NAME = "fileToWrite.txt";
+const FILE_PATH = join("src", "streams", "files", FILE_NAME);
+
 const write = async () => {
-  // Write your code here
+  try {
+    await pipeline(stdin, createWriteStream(FILE_PATH));
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 await write();
