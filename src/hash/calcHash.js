c9069b8339d6dcf731718dd37ec29435b277d326
ZhiznevskiyArtem <zhiznevskiyartem@gmail.com>
2025-07-17 21:27:39 +0300

    refactor: refactor calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,21 +1,21 @@
 const { createHash } = await import("node:crypto");
 import { createReadStream } from "node:fs";
 import { join } from "node:path";
+import { pipeline } from "node:stream/promises";
 
 const FILE_NAME = "fileToCalculateHashFor.txt";
 const FILE_PATH = join("src", "hash", "files", FILE_NAME);
-const hash = createHash("sha256");
-
-const input = createReadStream(FILE_PATH);
 
 const calculateHash = async () => {
-  input.on("readable", () => {
-    const data = input.read();
-    if (data) hash.update(data);
-    else {
-      console.log(`${hash.digest("hex")}`);
-    }
-  });
+  const hash = createHash("sha256");
+  const input = createReadStream(FILE_PATH);
+  try {
+    await pipeline(input, hash);
+
+    console.log(`${hash.digest("hex")}`);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 await calculateHash();
